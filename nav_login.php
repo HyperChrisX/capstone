479bbb165ce501d7e83edc7a380579a08e426a66
Christian Saltness <cj55448@hotmail.com>
2017-05-17 12:43:03 -0500

    changed nav bar brand to say home
    ^^^^

--- nav_login.php
+++ nav_login.php
@@ -8,7 +8,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="http://techyari.in"></a>
+          <a class="navbar-brand" href="index.php">Home</a>
         </div>
 
         <div class="navbar-collapse collapse">
