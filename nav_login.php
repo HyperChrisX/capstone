83018790a0be0645c5ed3999b8cf7e0b64cda33f
Christian Saltness <cj55448@hotmail.com>
2017-05-17 00:50:43 -0500

    new page for user logged in
    added logged in nav bar and a new version of userhome to be merged with
    the old version.

---
+++ nav_login.php
@@ -0,0 +1,22 @@
+<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="navbar-change">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://techyari.in"></a>
+        </div>
+
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><?php echo $_SESSION['sess_username'];?></a></li>
+            <li><a href="logout.php">Logout</a></li>
+          </ul>
+        </div>
+      </div>
+	</div>
+</div>
